b00d3ff6b6d705a97490a8a672dba89b473712e0
Liya_nazrin <liyanazrin2004@gmail.com>
2025-12-31 14:28:31 +0530

    Update cyber.py

--- cyber.py
+++ cyber.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pickle
 import base64
 import os
+from sklearn.ensemble import RandomForestClassifier
 def add_bg_from_local(image_file):
     script_dir = os.path.dirname(__file__)
     img_path = os.path.join(script_dir, image_file)
@@ -114,3 +115,4 @@ elif page=="📊 Output":
 
     else:
         st.warning("Please run a prediction first from the Input Data page.")
+
